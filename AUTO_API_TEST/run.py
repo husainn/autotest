90b5de143289f6dbca3d2182911435d432d0f6cc
husainn <975810720@qq.com>
2020-08-16 21:29:19 +0800

    update

--- AUTO_API_TEST/run.py
+++ AUTO_API_TEST/run.py
@@ -1,8 +1,14 @@
-import unittest
-from HTMLTestRunner_PY3.HTMLTestRunner_PY3 import HTMLTestRunner
+import sys
+import os
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+sys.path.append(curPath)
 
 from AUTO_API_TEST.common.pro_path import result_path
 from AUTO_API_TEST.common.test_http_request import TestHttpRequest
+import unittest
+from HTMLTestRunner_PY3.HTMLTestRunner_PY3 import HTMLTestRunner
 
 suite = unittest.TestSuite()
 loader = unittest.TestLoader()
@@ -12,3 +18,4 @@ if __name__ == '__main__':
     with open(result_path,'wb') as stream:
         runner = HTMLTestRunner(stream,title='hugetest')
         runner.run(suite)
+        print('ok')
